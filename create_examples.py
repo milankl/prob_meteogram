c837c67b90fbc3e26117cceae5d7cb8dbf85687d
milankl <milankloewer@gmx.de>
2018-06-11 17:47:54 +0100

    automatic creation of examples and png export

---
+++ create_examples.py
@@ -0,0 +1,13 @@
+import os, sys, inspect
+HERE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.append(HERE_PATH)
+
+locations = ["Santiago Chile",
+            "Kingston Jamaica",
+            "ECMWF Reading",
+            "Hobart Australia",
+            "Fairbanks Alaska"]
+
+for loc in locations:
+    os.system("python meteogram.py '"+loc+"' 1")
+    print("Meteogram created for "+loc)
